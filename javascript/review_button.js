74a5d967ff4913885dc80d2bc3a543872b978d67
martin <klm>
2021-03-28 01:11:43 -0700

    fixed review button
    now click 'write a review" to open/ close the review form

--- javascript/review_button.js
+++ javascript/review_button.js
@@ -1,16 +1,31 @@
 /* from W3C*/
 /*accordion */
-var reviewAcc = document.getElementById('reviewForm');
+// var reviewAcc = document.getElementById('reviewForm');
+var reviewBut = document.getElementsByClassName('reviewButton');
 var reviewF = document.getElementById('QA');
 
-reviewAcc.addEventListener("click", function(){
-    // reviewAcc.toggle("active");
-    // var panel = reviewAcc.nextElementSibling;
 
-    if(reviewF.style.display === "grid"){
-        reviewF.style.display = "none";
-    }
-    else{
-        reviewF.style.display = "grid";
-    }
-});
+for(var i=0; i<reviewBut.length; i++){
+    reviewBut[i].addEventListener("click", function(){
+        // reviewAcc.toggle("active");
+        // var panel = reviewAcc.nextElementSibling;
+        if(reviewF.style.display === "grid"){
+            reviewF.style.display = "none";
+        }
+        else{
+            reviewF.style.display = "grid";
+        }
+    });
+
+}
+
+// reviewBut.addEventListener("click", function(){
+//     // reviewAcc.toggle("active");
+//     // var panel = reviewAcc.nextElementSibling;
+//     if(reviewF.style.display === "grid"){
+//         reviewF.style.display = "none";
+//     }
+//     else{
+//         reviewF.style.display = "grid";
+//     }
+// });
