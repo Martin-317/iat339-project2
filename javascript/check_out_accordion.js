816b56c1f2f0d2cb123a2b19a70353c7c3cbd139
martin <klm>
2021-03-30 14:56:34 -0700

    check out page "edit button"
    fixed edit button's problem

--- javascript/check_out_accordion.js
+++ javascript/check_out_accordion.js
@@ -29,12 +29,14 @@ var shippingCheck = document.getElementById('shippingCheck');
 var shippingTitle = document.getElementById('shippingTitle');
 var shippingDrop = document.getElementById('shippingDrop');
 var shippingEdit = document.getElementById('shippingEdit');
+var shippingForm = document.getElementById('co_shipping_info');
 
 //billing
 var billingHeader = document.getElementById('billingHeader');
 var billingCheck = document.getElementById('billingCheck');
 var billingTitle = document.getElementById('billingTitle');
 var billingDrop = document.getElementById('billingDrop');
+var billingEdit = document.getElementById('billingEdit');
 
 //payment
 var paymentHeader = document.getElementById('paymentHeader');
@@ -53,7 +55,23 @@ shipping_next.addEventListener("click", function(){
     billing_check.style.display = "block";
     co_billing_info.style.display = "grid";
     // billing_info.style.display = "grid";
-    billing_next.style.display = "inline-block";
+    if(billingEdit.style.display === "inline-block"){
+        billing_next.style.display = "none";
+
+    }
+    else{
+        billing_next.style.display = "inline-block";
+    }
+
+
+    shippingCheck.style.display = "inline";
+    shippingTitle.innerHTML = "Shipping";
+    //billing part
+    billingHeader.classList.remove('co_subheader_unselected');
+    billingHeader.classList.add('co_subheader_selected');
+    billingDrop.src = "img/dropdown_icon_1.png";
+    //edite
+    shippingEdit.style.display = "inline-block";
 });
 
 
@@ -73,15 +91,19 @@ function show_hide_billing_info(){
 //billing next button 
 billing_next.addEventListener("click",function(){
     //hide shipping info
-    finished_shipping_info.style.display = "none";
+    // finished_shipping_info.style.display = "none";
     //hide billing info
     billing_check.style.display = "none";
     billing_form.style.display = "none";
-    billing_info.style.display = "none";
+    // billing_info.style.display = "none";
+    billing_info.style.display = "grid";
     billing_next.style.display = "none";
     //show card info
     paymentImg.style.display = "flex";
     cardInfo.style.display = "block";
+    //show edit
+    // shippingEdit.style.display = "inline-block";
+    billingEdit.style.display = "inline-block";
 });
 
 //cart&summary
@@ -128,21 +150,16 @@ function showBlock(){
 //image/text change
 //shipping
 
-shipping_next.addEventListener('click', function(){
-    shippingCheck.style.display = "inline";
-    shippingTitle.innerHTML = "Shipping";
-    //billing part
-    billingHeader.classList.remove('co_subheader_unselected');
-    billingHeader.classList.add('co_subheader_selected');
-    billingDrop.src = "img/dropdown_icon_1.png";
-    //edite
-    shippingEdit.style.display = "inline-block";
-});
-
-//billing
-shipping_next.addEventListener('click', function(){
-
-});
+// shipping_next.addEventListener('click', function(){
+//     // shippingCheck.style.display = "inline";
+//     // shippingTitle.innerHTML = "Shipping";
+//     // //billing part
+//     // billingHeader.classList.remove('co_subheader_unselected');
+//     // billingHeader.classList.add('co_subheader_selected');
+//     // billingDrop.src = "img/dropdown_icon_1.png";
+//     // //edite
+//     // shippingEdit.style.display = "inline-block";
+// });
 
 coNext.addEventListener('click', function(){
     billingCheck.style.display = "inline";
@@ -154,12 +171,23 @@ coNext.addEventListener('click', function(){
     shippingDrop.src = "img/dropdown_icon_horizontal_1.png";   
     billingDrop.src = "img/dropdown_icon_horizontal_1.png";
     //edit
-    shippingEdit.style.display = ('none');
+    // shippingEdit.style.display = ('none');
 });
 
 //shipping edit button
 shippingEdit.addEventListener('click', function(){
-    co_billing_info.style.display = "grid";
+    console.log('1');
+    shippingForm.style.display = "grid";
+    shipping_next.style.display = "block";
+    shippingEdit.style.display = "none";
+    finished_shipping_info.style.display = "none";
+});
+
+billingEdit.addEventListener('click', function(){
+    billing_form.style.display = "grid";
+    coNext.style.display = "block";
+    billingEdit.style.display = "none";
+    billing_info.style.display = "none";
 });
 
 //function call
